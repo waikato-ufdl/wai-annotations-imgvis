6d7d29353311178a98cbd2df67e445a752245f8c
Peter Reutemann <fracpete@gmail.com>
2022-07-14 09:32:30 +1200

    moved shapely polygon dependency and helper functions into core module

--- src/wai/annotations/imgvis/isp/combine_annotations/component/_CombineAnnotationsOD.py
+++ src/wai/annotations/imgvis/isp/combine_annotations/component/_CombineAnnotationsOD.py
@@ -9,57 +9,9 @@ from wai.annotations.core.component import ProcessorComponent
 from wai.annotations.core.stream import ThenFunction, DoneFunction
 from wai.annotations.core.stream.util import RequiresNoFinalisation
 from wai.annotations.domain.image.object_detection import ImageObjectDetectionInstance
-
-
-UNION = "union"
-INTERSECT = "intersect"
-COMBINATIONS = [
-    UNION,
-    INTERSECT,
-]
+from wai.annotations.core.util import INTERSECT, COMBINATIONS, to_polygons, intersect_over_union
 
 STREAM_INDEX = "stream_index"
-
-
-def to_polygons(located_objects):
-    """
-    Turns the located objects into shapely polygons.
-
-    :param located_objects: the objects to convert
-    :type located_objects: LocatedObjects
-    :return: the list of polygons
-    :rtype: list
-    """
-    result = []
-    for obj in located_objects:
-        coords = []
-        for point in obj.get_polygon().points:
-            coords.append((point.x, point.y))
-        result.append(Polygon(coords))
-    return result
-
-
-def intersect_over_union(poly1, poly2):
-    """
-    Calculates the IoU (intersect over union) for the two polygons.
-
-    :param poly1: the first polygon
-    :type poly1: Polygon
-    :param poly2: the second polygon
-    :type poly2: Polygon
-    :return: the IoU
-    :type: float
-    """
-    try:
-        intersection = poly2.intersection(poly1)
-        if intersection.area > 0:
-                union = unary_union([poly2, poly1])
-                return intersection.area / union.area
-        else:
-            return 0
-    except:
-        print("Failed to compute IoU!")
-        return 0
 
 
 class CombineAnnotationsOD(
